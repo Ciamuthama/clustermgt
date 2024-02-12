04aa1d5650194c9d92884a67fb2a95f98f097446
Peter Muthama <muthamapeter79@gmail.com>
2024-02-12 17:04:14 +0300

    Add: server functionality

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,38 @@
 const express = require("express");
-const morgan = require("morgan");
 const memberSchema = require("../schema/schema")
 
 const routes = express.Router();
- 
+
+routes.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http:localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    next();
+  });
+
 routes.get("/", (req, res) => {
     memberSchema.find()
     .then((results)=>res.json(results))
     .catch((err)=>console.log(err))
-    return res.status(200).json({ message: "Welcome to the API" });
+   
 });
+
+routes.post('/new', (req,res)=>{
+    const newMember = new memberSchema(req.body)
+
+    newMember.save()
+        .then((results)=>{
+             res.status(200).json( results, {message: "Member added successfully" + results})
+         })
+        .catch((error)=> console.log(error));
+})
+
+routes.get("/:id", (req, res) => {
+const id = req.params.id
+
+memberSchema.findById(id)
+.then((results)=>res.json(results))
+.catch((err)=>console.log(err))
+})
 
 module.exports = routes;
 
--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -3,39 +3,31 @@ const Schema = mongoose.Schema;
 
 const newSchema = new Schema({
   member_no: {
-    type: String,
-    required: true,
+    type: Number,
   },
   name: {
     type: String,
-    required: true,
   },
   id: {
     type: Number,
-    required: true,
   },
   telephone: {
     type: Number,
-    required: true,
   },
   date_of_join: {
     type: Date,
-    required: true,
   },
   district: {
     type: String,
-    required: true,
   },
   cluster: {
     type: String,
-    required: true,
   },
   cluster_leader: {
     type: String,
-    required: true,
   },
 });
 
 const memberSchema = mongoose.model("Member", newSchema);
 
-module.exports = memberSchema
+module.exports = memberSchema;
