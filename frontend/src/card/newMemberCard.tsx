3205d54cc57d6e15653a06caf25132c6cc519839
Peter Muthama <muthamapeter79@gmail.com>
2024-02-14 14:26:55 +0300

    add: image input

--- frontend/src/card/newMemberCard.tsx
+++ frontend/src/card/newMemberCard.tsx
@@ -1,5 +1,4 @@
-import { Label, TextInput } from "flowbite-react";
-
+import { FileInput, Label, TextInput } from "flowbite-react";
 
 export default function NewMemberCard() {
   return (
@@ -7,93 +6,101 @@ export default function NewMemberCard() {
       <div className=" flex h-screen bg-white">
         <div>
           <form className="grid grid-cols-2 gap-2">
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="member_no" value="Your email" />
-                </div>
-                <TextInput
-                  id="member_no"
-                  type="text"
-                  placeholder="member name"
-                  required
-                />
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="member_no" value="Your email" />
               </div>
+              <TextInput
+                id="member_no"
+                type="text"
+                placeholder="member name"
+                required
+              />
+            </div>
 
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="member_no" value="Your email" />
-                </div>
-                <TextInput
-                  id="member_no"
-                  type="text"
-                  placeholder="name@company.com"
-                  required
-                />
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="member_no" value="Your email" />
               </div>
+              <TextInput
+                id="member_no"
+                type="text"
+                placeholder="name@company.com"
+                required
+              />
+            </div>
 
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="member_no" value="Your email" />
-                </div>
-                <TextInput
-                  id="member_no"
-                  type="text"
-                  placeholder="name@company.com"
-                  required
-                />
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="member_no" value="Your email" />
               </div>
+              <TextInput
+                id="member_no"
+                type="text"
+                placeholder="name@company.com"
+                required
+              />
+            </div>
 
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="member_no" value="Your email" />
-                </div>
-                <TextInput
-                  id="member_no"
-                  type="text"
-                  placeholder="name@company.com"
-                  required
-                />
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="member_no" value="Your email" />
               </div>
+              <TextInput
+                id="member_no"
+                type="text"
+                placeholder="name@company.com"
+                required
+              />
+            </div>
 
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="member_no" value="Your email" />
-                </div>
-                <TextInput
-                  id="member_no"
-                  type="text"
-                  placeholder="name@company.com"
-                  required
-                />
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="member_no" value="Your email" />
               </div>
+              <TextInput
+                id="member_no"
+                type="text"
+                placeholder="name@company.com"
+                required
+              />
+            </div>
 
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="member_no" value="Your email" />
-                </div>
-                <TextInput
-                  id="member_no"
-                  type="text"
-                  placeholder="name@company.com"
-                  required
-                />
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="member_no" value="Your email" />
               </div>
+              <TextInput
+                id="member_no"
+                type="text"
+                placeholder="name@company.com"
+                required
+              />
+            </div>
 
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="member_no" value="Your email" />
-                </div>
-                <TextInput
-                  id="member_no"
-                  type="text"
-                  placeholder="name@company.com"
-                  required
-                />
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="member_no" value="Your email" />
               </div>
-
-              <div>
-
+              <TextInput
+                id="member_no"
+                type="text"
+                placeholder="name@company.com"
+                required
+              />
+            </div>
+            <div id="fileUpload" className="max-w-md">
+              <div className="mb-2 block">
+                <Label htmlFor="file" value="Upload file" />
               </div>
+              <FileInput
+                id="file"
+                helperText="Add Profile Picture"
+                accept="image/png, image/jpeg" 
+              />
+            </div>
+
+            <div></div>
           </form>
         </div>
       </div>
